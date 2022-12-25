c89aa5f9852611738af0bb7eb141fbda01340a5a
NMarkovic98 <pomocnih@gmail.com>
2022-12-25 23:40:36 +0100

    convert next auth provider to typescript

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,16 @@
+import NextAuth from 'next-auth';
+import FacebookProvider from 'next-auth/providers/facebook';
+const { FACEBOOK_CLIENT_ID = '', FACEBOOK_CLIENT_SECRET = '' } = process.env;
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    FacebookProvider({
+      clientId: FACEBOOK_CLIENT_ID,
+      clientSecret: FACEBOOK_CLIENT_SECRET,
+    }),
+    // ...add more providers here
+  ],
+};
+
+export default NextAuth(authOptions);
