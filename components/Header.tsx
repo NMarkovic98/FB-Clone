2571bb379b5fc9af7b0a554908dfa05c5b408b8f
NMarkovic98 <pomocnih@gmail.com>
2023-01-03 22:59:12 +0100

    fix styles

--- components/Header.tsx
+++ components/Header.tsx
@@ -22,33 +22,27 @@ import {
 function Header() {
   const { data: session } = useSession();
 
-  const runSignOut = () => {
-    signOut();
-  };
-
   return (
-    <div className="sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md">
-      {/* left */}
-
+    <header className="sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md">
+      {/* Left */}
       <div className="flex items-center">
         <Image
-          width={40}
-          height={40}
+          src="https://links.papareact.com/5me"
+          width="40"
+          height="40"
           layout="fixed"
           alt="facebook image"
-          src="https://links.papareact.com/5me"
         />
-
-        <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
+        <div className="hidden md:inline-flex ml-2 items-center rounded-full bg-gray-100 p-2">
           <SearchIcon className="h-6 text-gray-600" />
           <input
-            className="hidden md:inline-flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink"
-            type="text"
+            className="hidden lg:inline-flex ml-2 bg-transparent outline-none placeholder-gray-500 flex-shrink"
             placeholder="Search Facebook"
           />
         </div>
       </div>
-      {/* center */}
+
+      {/* Center */}
       <div className="flex justify-center flex-grow">
         <div className="flex space-x-6 md:space-x-2">
           <HeaderIcon active Icon={HomeIcon} />
@@ -58,27 +52,27 @@ function Header() {
           <HeaderIcon Icon={UserGroupIcon} />
         </div>
       </div>
-      {/* right */}
+
+      {/* Right */}
       <div className="flex items-center sm:space-x-2 justify-end">
-        {/* Profile pic */}
         <Image
+          onClick={() => signOut()}
           className="rounded-full cursor-pointer"
-          alt="Profile Image"
           src={session?.user?.image || ''}
-          width={40}
-          height={40}
+          width="40"
+          height="40"
           layout="fixed"
-          onClick={runSignOut}
+          alt="profile image"
         />
-        <p className="whitespace-nowrap font-semibold pr-3">
-          {session?.user?.name}
+        <p className="hidden lg:inline-flex text-sm whitespace-nowrap font-semibold pr-3">
+          {session?.user?.name || ''}
         </p>
         <ViewGridIcon className="icon" />
         <ChatIcon className="icon" />
         <BellIcon className="icon" />
         <ChevronDownIcon className="icon" />
       </div>
-    </div>
+    </header>
   );
 }
 
