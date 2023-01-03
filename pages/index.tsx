38cbc4da87c995049ff48275e751fb460c9b8966
NMarkovic98 <pomocnih@gmail.com>
2023-01-03 22:58:18 +0100

    add feed and fix styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,22 +4,23 @@ import { getSession } from 'next-auth/react';
 import Login from '../components/Login';
 import Header from '../components/Header';
 import SideBar from '../components/Sidebar';
+import Feed from '../components/Feed';
 
 export default function Home({ session }: { session: any }) {
   if (!session) return <Login />;
   return (
-    <div>
+    <div className="h-screen bg-gray-100 overflow-hidden">
       <Head>
         <title>Facebook</title>
       </Head>
 
       <Header />
-      <main>
-        <SideBar />
-      </main>
 
-      {/* feed */}
-      {/* widgets */}
+      <main className="flex">
+        <SideBar />
+        <Feed />
+        {/* <Widgets /> */}
+      </main>
     </div>
   );
 }
